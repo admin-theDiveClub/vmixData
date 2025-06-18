a228662d0718c2f8195169d7e83a4ead7e5188f7
UV_StrixLaptop <yuvannaidoo@gmail.com>
2025-06-18 16:55:44 +0200

    Test

--- netlify/functions/vmix.js
+++ netlify/functions/vmix.js
@@ -2,6 +2,9 @@ exports.handler = async function(event, context) {
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ message: "Hello from Netlify Function" })
+    body: JSON.stringify([
+      { "Text1": "Yuvan", "Text2": 14 },
+      { "Text1": "Gershwin", "Text2": 9 }
+    ])
   }
 }
