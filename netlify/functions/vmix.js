a426bac5ce81ff61b9c036a28c42f809ed498ee9
UV_StrixLaptop <yuvannaidoo@gmail.com>
2025-06-19 08:25:49 +0200

    Test

--- netlify/functions/vmix.js
+++ netlify/functions/vmix.js
@@ -1,10 +1,30 @@
-exports.handler = async function(event, context) {
-  return {
+exports.handler = async function(event, context) 
+{
+  // Initialize Supabase client
+  const { createClient } = window.supabase;
+  const supabaseUrl = 'https://db.thediveclub.org';
+  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tdmx3dXRudW91eW51a290ZGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2OTgyMzEsImV4cCI6MjA1NTI3NDIzMX0.qyEDq8w67G2BMfyHO7Iyvd3nFUSd0sulJhGl0eGkbfA';
+  const supabase = createClient(supabaseUrl, supabaseKey);
+
+  var data = 
+  [
+    { "Player": "Yuvan", 
+      "Score": 14 
+    },
+    { "Player": "Gershwin", 
+      "Score": 9 
+    }
+  ];
+
+  const response = await supabase.from('tbl_players').select('*').eq('username', 'yuvannaidoo@gmail.com');
+  console.log("GetPlayer Response:", response.data[0]);
+  data.push({response: response.data[0]});
+
+  response = 
+  {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify([
-      { "Text1": "Yuvan", "Text2": 14 },
-      { "Text1": "Gershwin", "Text2": 9 }
-    ])
+    body: JSON.stringify(data)
   }
+  return response;
 }
