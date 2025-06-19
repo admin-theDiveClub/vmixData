7fefb75a0ccc517b0bf2dd1ad2eb57d5f64a3e15
UV_StrixLaptop <yuvannaidoo@gmail.com>
2025-06-19 08:29:16 +0200

    Fix

--- netlify/functions/vmix.js
+++ netlify/functions/vmix.js
@@ -1,7 +1,8 @@
-exports.handler = async function(event, context) 
+import { createClient } from '@supabase/supabase-js';
+
+export const handler = async function() 
 {
   // Initialize Supabase client
-  const { createClient } = window.supabase;
   const supabaseUrl = 'https://db.thediveclub.org';
   const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tdmx3dXRudW91eW51a290ZGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2OTgyMzEsImV4cCI6MjA1NTI3NDIzMX0.qyEDq8w67G2BMfyHO7Iyvd3nFUSd0sulJhGl0eGkbfA';
   const supabase = createClient(supabaseUrl, supabaseKey);
