8e14e8ac26ffed40563f192a5fcf0c615f2dae03
UV_StrixLaptop <yuvannaidoo@gmail.com>
2025-07-03 13:29:07 +0200

    Test

--- netlify/functions/vmix_SPL_liveMatch.js
+++ netlify/functions/vmix_SPL_liveMatch.js
@@ -55,7 +55,52 @@ exports.handler = async function(event, context) {
   {
     const liveMatches = await fetchMatches(); 
 
-    const data = liveMatches;
+    const match = liveMatches[0];
+
+    const player_Home = match.players.h.fullName;
+    const player_Home_firstName = match.players.h.fullName.split(' ')[0];
+    const H_fw = match.results.h.fw;
+    const H_bf = match.results.h.bf;
+    const H_gb = match.results.h.gb;
+    const H_rf = match.results.h.rf;
+    const H_b_in = match.results.h.breaks.in;
+    const H_b_dry = match.results.h.breaks.dry;
+    const H_b_scr = match.results.h.breaks.scr;
+
+    const player_Away = match.players.a.fullName;
+    const player_Away_firstName = match.players.a.fullName.split(' ')[0];
+    const A_fw = match.results.a.fw;
+    const A_bf = match.results.a.bf;
+    const A_gb = match.results.a.gb;
+    const A_rf = match.results.a.rf;
+    const A_b_in = match.results.a.breaks.in;
+    const A_b_dry = match.results.a.breaks.dry;
+    const A_b_scr = match.results.a.breaks.scr;
+
+    var liveMatchStripped = {
+      player_Home,
+      player_Home_firstName,
+      H_fw,
+      H_bf,
+      H_gb,
+      H_rf,
+      H_b_in,
+      H_b_dry,
+      H_b_scr,
+      player_Away,
+      player_Away_firstName,
+      A_fw,
+      A_bf,
+      A_gb,
+      A_rf,
+      A_b_in,
+      A_b_dry,
+      A_b_scr
+    };
+
+    const data = liveMatchStripped;
+
+
 
     const response =
     {
