72bb0936eeafe1824ba23be294f545b2ca123b0b
UV_StrixLaptop <yuvannaidoo@gmail.com>
2025-07-10 12:38:11 +0200

    Test

---
+++ netlify/functions/vmix_SPL_liveTimer.js
@@ -0,0 +1,83 @@
+const https = require('https')
+
+exports.handler = async function(event, context) {
+  const supabaseUrl = 'https://db.thediveclub.org';
+  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tdmx3dXRudW91eW51a290ZGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2OTgyMzEsImV4cCI6MjA1NTI3NDIzMX0.qyEDq8w67G2BMfyHO7Iyvd3nFUSd0sulJhGl0eGkbfA';
+
+
+  const fetchMatches = () => new Promise
+  (
+    (resolve, reject) => 
+    {
+        var _table = 'tbl_matches';
+
+        // Build query parameters for filtering
+        const queryParams = new URLSearchParams({
+            'info->>status': 'eq.Live',
+            'competitions->>leagueID': 'eq.74f79467-9c26-421b-bcef-389bb40fe1ad'
+        }).toString();
+
+        const options = 
+        {
+        hostname: 'db.thediveclub.org',
+        path: `/rest/v1/${_table}?${queryParams}`,
+        method: 'GET',
+        headers: 
+        {
+            apikey: supabaseKey,
+            Authorization: `Bearer ${supabaseKey}`,
+            'Content-Type': 'application/json',
+            Prefer: 'return=representation'
+        }
+        };
+
+        const req = https.request(options, res => 
+        {
+            let body = '';
+            res.on('data', function(chunk) 
+            {
+            body += chunk;
+            });
+
+            res.on('end', function() 
+            {
+            const data = JSON.parse(body);
+            resolve(data);
+            });
+        });
+
+        req.on('error', err => reject(err));
+        req.end();
+    }
+  );
+
+  try 
+  {
+    const liveMatches = await fetchMatches(); 
+
+    const match = liveMatches[0];
+    const matchID = match.id;
+
+    const data = matchID;
+
+
+
+    const response =
+    {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    };
+
+    return response;
+  
+    } catch (err) 
+    {
+      const error = 
+      {
+        statusCode: 500,
+        body: JSON.stringify({ error: err.message })
+      };
+      return error;
+    }
+};
